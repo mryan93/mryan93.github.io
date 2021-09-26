eb2f59cafcbd822c82d47a810fca3049432e0ab5
mryan93 <mwieser@uncc.edu>
2021-09-26 17:04:05 -0400

    Added style sheets, welcome.html

---
+++ itis3135/scripts/alert.js
@@ -0,0 +1,2 @@
+alert("Hey my script is running");
+var myVar = prompt("What is your name?");
---
+++ itis3135/scripts/welcome-script.js
@@ -0,0 +1,4 @@
+var fName = prompt("Welcome to ManiacRazorbackWombat LLC! What is your first name?");
+var lName = prompt("What is your last name?");
+var fullName = fName + " " + lName;
+var mood = prompt("How are you doing today?");
